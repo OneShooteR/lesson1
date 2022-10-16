fdcba97c036845b07544e52474be7e9f436b8a9d
OneShooteR <latygin.evgeniy@gmail.com>
2022-10-16 21:40:26 +0700

    version1.1

--- homework2.py
+++ homework2.py
@@ -4,7 +4,7 @@ n = 0
 j = 0
 c = 0
 c1 = 0
-c2 = 0
+
 def isfloat(value):
         try:
             float(value)
@@ -19,7 +19,7 @@ def isint(value):
     except ValueError:
             return False
 
-while float(i) < 3 and float(i) < 5:
+while float(i) < 3 or float(i) > 5:
     print("Сколько элементов в вашем списке?(3-5) ")
     i = input()
 while n < float(i):
@@ -32,16 +32,13 @@ while j < float(i):
         c += 1
     if isint(s[j]) == True:    
         c1 += 1
-    if isfloat(s[j]) == True and '.' in s[j] or isint(s[j]) == True:
-        c2 += 1
     j += 1    
 if (float(i) - c) == 0:
     print("в этом списке все элементы типа float") 
 if (float(i)- c1) == 0:
     print("в этом списке все элементы типа int")
-if (float(i)- c2) == 0:
-    print("в этом списке есть элементы типа int И float")    
-else:
-    print("в этом списке есть элементы типа str")         
+if (float(i)- c) != 0 and (float(i)- c1) != 0 :
+    print("в этом списке элементы различного типа")    
+     
 
    
